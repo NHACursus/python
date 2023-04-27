34ceab650320d3331b35f522bc391dde24254d25
Jeroen <jeroen.berlee@outlook.com>
2023-04-28 01:17:29 +0200

    "updated to lesson 7 start"

---
+++ Les7/Les7.py
@@ -0,0 +1,15 @@
+# 7.2 what is a string (repetition) some folks need 
+
+
+# A few examples
+string1 = ""
+string2 = "A"
+string3 = "123ABC"
+string4 = "Python is een taal die begin jaren 90 ontworpen werd."
+string5 = " @&*()!_)_@*&#(!_!" 
+
+
+# 7.3 string = list of characters
+
+# A list looks like this, for example
+my_cart = ["appel", "banaan", "citroen"]
