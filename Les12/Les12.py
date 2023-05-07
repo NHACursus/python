de8b8381af98f80ca42d11ca00a3d364cbcf65b7
Jeroen <jeroen.berlee@outlook.com>
2023-05-07 17:58:51 +0200

    Lesson 12

---
+++ Les12/Les12.py
@@ -0,0 +1,10 @@
+#        print(first, second)
+
+def equal_to(first = int, second = int):
+    
+    if (first != second):
+       return False
+    else: return True
+
+
+print( equal_to( 10, 11) )
